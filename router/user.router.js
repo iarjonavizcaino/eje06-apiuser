317ee188dd88c72c54134fd4904ab7b09b9b758d
Israel Arjona <iarjona@ittepic.edu.mx>
2017-10-13 18:32:03 -0600

    V0.9

--- router/user.router.js
+++ router/user.router.js
@@ -3,13 +3,15 @@ const handler = require('../utils/handler');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/unidad02', {
+mongoose.connect('mongodb://localhost:27017/unidad2', {
   useMongoClient: true
 });
 
 const User = require('../models/user.model');
 
 module.exports = () => {
+
+    
 
     router.get('/', (req, res) => {
         User.find({})
@@ -21,7 +23,45 @@ module.exports = () => {
         const id = req.params.id;
         User.find({_id:id})
         .sort()
-        .exec(handler.handleMany.bind(null, 'users', res));
+        .exec(handler.handleOne.bind(null, 'user', res));
+    });
+
+    /* ---Inserciones---- */
+
+    router.post('/', (req, res) => {
+
+        //Recibir parametros
+        const usuario = req.body;
+
+        User.create(usuario)
+            .then( 
+                function(data) {
+                    console.log(data);
+                    res.json(data);
+                }
+            )
+            .catch(
+                function (err) {
+                    console.log(err);
+                    res.status(400);
+                    res.json({error:err});
+                }
+            );
+    });
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+
+        User.remove({_id:id}, function(err, data) {
+            if(err) {
+                console.log(err);
+                res.status(400);
+                res.json({error:err});
+            } else {
+                res.json({msj: "Todo estuvo bien"});
+            }
+        });
+
     });
 
     return router;
